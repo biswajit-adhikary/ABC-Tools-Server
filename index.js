5ea0fd96bb75d3bd6e9feb73a21b1787b951f7f1
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-22 22:25:00 +0600

    JWT added & API created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 // Set Port
 const port = process.env.PORT || 5000;
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 // Set Middleware
 app.use(cors());
 app.use(express.json());
+
+// JWT token verify
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ Message: 'Unauthorized Access!' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 // Connect DB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhlp5.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,6 +37,15 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db('abctools').collection('tools');
+
+        // JWT token
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         // API for get tools
         app.get('/tools', async (req, res) => {
@@ -35,9 +61,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Front-End Server Running!');
+    res.send('Back-End Server Running!');
 });
 
 app.listen(port, () => {
-    console.log('Console Server Running!');
+    console.log('Console Running!');
 });
