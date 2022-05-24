6b6f317ef3f88da3866fac67c682fdc9c4e895b9
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-25 02:10:40 +0600

    Order, my-order & JWT added

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// // JWT token verify
-// function verifyToken(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ Message: 'Unauthorized Access!' });
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden Access' });
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
+// JWT token verify
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ Message: 'Unauthorized Access!' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 // Connect DB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jhlp5.mongodb.net/?retryWrites=true&w=majority`;
@@ -39,6 +39,7 @@ async function run() {
         const toolsCollection = client.db('abctools').collection('tools');
         const reviewCollection = client.db('abctools').collection('reviews');
         const userCollection = client.db('abctools').collection('users');
+        const orderCollection = client.db('abctools').collection('orders');
 
         // // JWT token
         // app.post('/login', async (req, res) => {
@@ -87,6 +88,48 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
         })
+
+
+        // API for create order
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.send(result);
+        })
+
+        // API for all order
+        app.get('/orders', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+        // Get single order
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.findOne(query);
+            res.send(result);
+        });
+
+
+        // Get My Order API
+        app.get('/my-orders', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+        })
+
 
     } finally { }
 }
